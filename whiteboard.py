a3d688e808c404c1fd1b3abbff64e17625e74c00
Brian <bstanton773@gmail.com>
2023-09-26 10:12:24 -0500

    Whiteboard

---
+++ whiteboard.py
@@ -0,0 +1,9 @@
+# Given a string of letters, write a function to see if the word  (case insensitive) is a palindrome (the same word spelled forward and backwards).
+# The given string will contain only letters 
+# Examples:
+# is_palindrome("RaceCar") \\ => True
+# is_palindrome("mom")  \\ => True
+# is_palindrome("Shoha") \\ => False
+
+def solution(letters):
+    return letters.lower() == letters.lower()[::-1]
